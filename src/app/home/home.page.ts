0c088b23e238b37c0bf4760107280ed9a5736376
sachin <heshanf45@gmail.com>
2023-04-20 12:02:52 +0530

    correction of imports places

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule,QRCodeModule],
 })
+
+
 export class HomePage {
 
     // https://www.npmjs.com/package/angularx-qrcode
